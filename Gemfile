5f6b9b253957de7f5203cb8c46b43ba4747cdec2
Vitor Matta <vitor@cyberlabs.ai>
2020-10-01 23:04:53 -0300

    Validate json input paths and testing cossine, levenshtein and jaro-winkler similarities and distance

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source 'https://rubygems.org'
 
 
 gem 'OptionParser', '~> 0.5.1'
+gem 'string-similarity'
+gem 'fuzzy-string-match'
+gem 'jaro_winkler', '~> 1.4'
--- compare-json.rb
+++ compare-json.rb
@@ -1,4 +1,7 @@
 require 'optparse'
+require 'string/similarity'
+require 'fuzzystringmatch'
+require 'jaro_winkler'
 
 # Receives two arguments indicating the path of the json files to compare
 # opens the files, compare and return the similarity
@@ -18,4 +21,31 @@ OptionParser.new do |opts|
     
 end.parse!
 
+json_path_1 = ARGV[0]
+json_path_2 = ARGV[1]
 
+#validate files
+if !File.file?(json_path_1)
+    puts "'#{json_path_1}' is not a valid file or does not exist"
+    exit(-1)
+end
+
+if !File.file?(json_path_2)
+    puts "'#{json_path_2}' is not a valid file or does not exist"
+    exit(-1)
+end
+
+json_1 = File.read(json_path_1)
+json_2 = File.read(json_path_2)
+
+
+cossine_sim = String::Similarity.cosine json_1, json_2
+levenshtein_similarity = String::Similarity.levenshtein json_1, json_2
+jarow_sim = FuzzyStringMatch::JaroWinkler.create( :pure ).getDistance json_1, json_2
+jw_dist = JaroWinkler.distance json_1, json_2
+
+
+p cossine_sim
+p levenshtein_similarity
+p jarow_sim
+p jw_dist
